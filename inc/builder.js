c4ac6e6411b00b108e33281cbf4fbbdaa1870d69
Alex Gusev (Asus N73SV) <alex@lancer64.com>
2017-07-10 08:34:57 +0300

    WF-88 - Create tables with dependencies

--- inc/builder.js
+++ inc/builder.js
@@ -16,28 +16,82 @@ function utilConvertFullName(name) {
 }
 
 /**
+ * Start build session.
+ * @param ctx
+ */
+function sessionStart(ctx) {
+    return new Promise(function (resolve, reject) {
+        let cfg = ctx.cfg;
+        let knex = knexLib(cfg.knex);
+        ctx.knex = knex;
+        resolve(ctx);
+    });
+}
+/**
+ * Close DB connections.
+ *
+ * @param ctx
+ * @returns {Promise}
+ */
+function sessionClose(ctx) {
+    return new Promise(function (resolve, reject) {
+        const knex = ctx.knex;
+        knex.destroy(() => {
+            console.log('knex is done');
+        });
+    });
+}
+
+function createTables(ctx) {
+    const tables = ctx.db.tables;
+    let current = ctx.currentTableIndex;
+    if (current == undefined) {
+        current = 0;
+        ctx.currentTableIndex = current;
+    }
+    if (current < tables.length) {
+        /* continue loop of promises */
+        return new Promise(function (resolve, reject) {
+            const knex = ctx.knex;
+            const tables = ctx.db.tables;
+            let current = ctx.currentTableIndex;
+            const tblName = tables[current];
+            console.log('Current table: %s.', tblName);
+            current += 1;
+            ctx.currentTableIndex = current;
+            resolve(ctx);
+        }).then(createTables);
+    } else {
+        /* end loop of promises */
+        return ctx;
+    }
+}
+/**
  * Load DEM JSON and parse it. Save parsed data into context.
  *
  * @param {builder.context} ctx
  */
 function exec(ctx) {
-    let cfg = ctx.cfg;
-    /** @type {data.db} */
-    let db = ctx.db;
-    let knex = knexLib(cfg.knex);
-    let tables = db.tables;
-    for (let one of tables) {
-        let tbl = utilConvertFullName(one);
-        knex.schema.createTableIfNotExists(tbl, function (table) {
-            table.increments();
-            table.string('name');
-            table.timestamps();
-        }).finally(function () {
-            knex.destroy(function () {
-                console.log('Done');
-            });
-        });
-    }
+    sessionStart(ctx)
+        .then(createTables)
+        .then(sessionClose);
+    // let cfg = ctx.cfg;
+    // /** @type {data.db} */
+    // let db = ctx.db;
+    // let knex = knexLib(cfg.knex);
+    // let tables = db.tables;
+    // for (let one of tables) {
+    //     let tbl = utilConvertFullName(one);
+    //     knex.schema.createTableIfNotExists(tbl, function (table) {
+    //         table.increments();
+    //         table.string('name');
+    //         table.timestamps();
+    //     }).finally(function () {
+    //         knex.destroy(function () {
+    //             console.log('Done');
+    //         });
+    //     });
+    // }
 
 }
 
